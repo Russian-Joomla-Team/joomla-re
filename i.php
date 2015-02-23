753d88b00d0089eddd07e809e3237c32c9bc9719
Администратор <@wjn>
2015-02-23 18:00:31 +0600

    Merge origin/master
    Conflicts:
    	test
    что то новое/ other new

---
+++ i.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @version build $ 0.0.0.2 17:56:49 23.02.2015
+ * */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+phpinfo();
